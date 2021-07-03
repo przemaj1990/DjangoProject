4d51a56194751a104d3b6996f78ec325fd1fbe67
Przemysław Majdański <przemyslawmajdanski@MacBook-Pro-4.local>
2021-07-03 12:43:44 +0200

    Start New Porject - additional changes #1

---
+++ Reasume.py
@@ -0,0 +1,13 @@
+# source env/bin/activate
+# echo env/ >> .gitignore 
+# ip install pip --upgrade
+# pip install Django
+# pip3 freeze > requirements.txt
+# django-admin startproject DjangoProject .
+# git add .
+# git status
+# git commit -m 'Start New Porject'
+# git push
+# python3.8 manage.py runserver
+# python manage.py makemigrations
+# python manage.py migrate
